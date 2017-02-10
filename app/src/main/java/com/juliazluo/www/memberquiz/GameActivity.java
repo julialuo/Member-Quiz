aaf30b7dd3adf2aa3b4dcad3bc0d88a0ef38fd18
Julia Luo <juliazhiyuan.luo@gmail.com>
2017-02-09 17:29:49 -0800

    Added contact and timer functionality

--- app/src/main/java/com/juliazluo/www/memberquiz/GameActivity.java
+++ app/src/main/java/com/juliazluo/www/memberquiz/GameActivity.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.media.Image;
+import android.os.CountDownTimer;
+import android.provider.ContactsContract;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -23,10 +25,12 @@ public class GameActivity extends AppCompatActivity {
 
     final Random random = new Random();
     ImageView imageView;
-    TextView scoreTxt;
+    TextView scoreTxt, countTxt;
     Button [] choiceBtns;
-    int score = 0;
+    int score = 0, timeSec = 0;
+    long timeMS = 0;
     int memberNum;
+    boolean returnFromContacts = false;
     String[] members = {"Jessica Cherny", "Kevin Jiang", "Jared Gutierrez", "Kristin Ho",
             "Christine Munar", "Mudit Mittal", "Richard Hu", "Shaan Appel", "Edward Liu",
             "Wilbur Shi", "Young Lin", "Abhinav Koppu", "Abhishek Mangla  Akkshay Khoslaa",
@@ -38,6 +42,7 @@ public class GameActivity extends AppCompatActivity {
             "Kedar Thakkar", "Leon Kwak", "Mohit Katyal", "Rochelle Shen", "Sayan Paul",
             "Sharie Wang", "Shreya Reddy", "Shubham Goenka", "Victor Sun", "Vidya Ravikumar"};
     String[] imageNames;
+    CountDownTimer count;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,6 +56,7 @@ public class GameActivity extends AppCompatActivity {
 
         imageView = (ImageView) findViewById(R.id.image);
         scoreTxt = (TextView) findViewById(R.id.score_txt);
+        countTxt = (TextView) findViewById(R.id.count_txt);
 
         choiceBtns = new Button[4];
         choiceBtns[0] = (Button) findViewById(R.id.btn1);
@@ -66,15 +72,19 @@ public class GameActivity extends AppCompatActivity {
                 @Override
                 public void onClick(View v) {
                     String answer = ((Button) v).getText().toString();
-                    checkAnswer(answer);
+                    checkAnswer(answer, false);
+                    count.cancel();
                     refreshScreen();
                 }
             });
         }
 
+        //quit game functionality
         ((Button) findViewById(R.id.quit_btn)).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //pause timer
+                count.cancel();
                 AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(GameActivity.this);
 
                 // set title
@@ -94,6 +104,7 @@ public class GameActivity extends AppCompatActivity {
                         .setNegativeButton("No",new DialogInterface.OnClickListener() {
                             public void onClick(DialogInterface dialog,int id) {
                                 dialog.cancel();
+                                createTimer(timeMS);
                             }
                         });
 
@@ -101,10 +112,34 @@ public class GameActivity extends AppCompatActivity {
                 alertDialog.show();
             }
         });
+
+        //clicking image creates new contact
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Creates a new Intent to insert a contact
+                Intent intent = new Intent(ContactsContract.Intents.Insert.ACTION);
+                // Sets the MIME type to match the Contacts Provider
+                intent.setType(ContactsContract.RawContacts.CONTENT_TYPE);
+                intent.putExtra(ContactsContract.Intents.Insert.NAME, members[memberNum]);
+                startActivity(intent);
+                count.cancel();
+                returnFromContacts = true;
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (returnFromContacts) {
+            createTimer(timeMS);
+            returnFromContacts = false;
+        }
     }
 
     protected void refreshScreen() {
-        //Change image and button text
+        //Change image, button text, and restart timer
         memberNum = random.nextInt(members.length);
         imageView.setImageDrawable
                 (   getResources().getDrawable(getResourceID(imageNames[memberNum], "drawable",
@@ -124,14 +159,21 @@ public class GameActivity extends AppCompatActivity {
                 randNums[i] = randNum;
             }
         }
+
+        createTimer(5000);
     }
 
-    protected void checkAnswer(String answer) {
+    protected void checkAnswer(String answer, boolean outOfTime) {
         if (answer == members[memberNum]) {
             score += 1;
             refreshScoreText();
         } else {
-            String wrongText = "You suck! The correct answer was " + members[memberNum];
+            String wrongText;
+            if (!outOfTime) {
+                wrongText = "You suck! The correct answer was " + members[memberNum];
+            } else {
+                wrongText = "You ran out of time! The correct answer was " + members[memberNum];
+            }
             Toast toast = Toast.makeText(getApplicationContext(), wrongText, Toast.LENGTH_SHORT);
             toast.show();
         }
@@ -139,6 +181,26 @@ public class GameActivity extends AppCompatActivity {
 
     protected void refreshScoreText() {
         scoreTxt.setText("Score: " + score);
+    }
+
+    protected void createTimer(long startMS) {
+        count = new CountDownTimer(startMS, 250) {
+
+            public void onTick(long ms) {
+                timeMS = ms;
+                if (Math.round((float)ms / 1000.0f) != timeSec) {
+                    timeSec = Math.round((float)ms / 1000.0f);
+                    countTxt.setText(timeSec + "");
+                }
+                Log.i("Time", "" + ms);
+            }
+
+            public void onFinish() {
+                checkAnswer("Out of time uh oh", true);
+                refreshScreen();
+            }
+        };
+        count.start();
     }
 
     protected final static int getResourceID
